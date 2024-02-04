f9f32fc9a64f1b30dbb56fae4b69d7e98068e874
Henrique Lindozo <138804485+devr1q@users.noreply.github.com>
2024-02-04 15:31:25 -0300

    testando

--- javascript/script.js
+++ javascript/script.js
@@ -7,9 +7,35 @@ let marks = [];
 
 const formatTime = (time) => {
     const hours = Math.floor(time / 360000);
-    const minuts = Math.floor(time / 360000) / 6000;
-    const seconds = Math.floor(time / 360000) / 6000 / 100;
+    const minutes = Math.floor((time % 360000) / 6000);
+    const seconds = Math.floor((time % 6000) / 100);
     const hundredths = time % 100;
 
-    return `${hours.toString().padStart(2, '0')}:${minuts.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}:${hundredths.toString().padStart(2, '0')}`;
+    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}:${hundredths.toString().padStart(2, '0')}`;
 }
+
+const toggleTimer = () => {
+    const button = document.getElementById('power');
+    const action = document.getAttribute('action');
+
+    clearInterval(intervelId);
+
+    if (action == 'start' || action == 'continue') {
+        intervelId = setInterval(() => {
+            timer += 1;
+            setTimer(timer);
+        }, 10);
+        button.setAttribute('action', 'pause');
+        button.innerHTML = '<i class="fa-solid fa-pause"></i>';
+    }
+    else if (action == 'pause') {
+        button.setAttribute('action', 'continue');
+        button.innerHTML = '<i class="fa-solid fa-play"></i>';
+    }
+}
+
+const setTimer = (time) => {
+    timerEl.innerText = formatTime(time);
+}
+
+document.getElementById('power').addEventListener('click', toggleTimer);
